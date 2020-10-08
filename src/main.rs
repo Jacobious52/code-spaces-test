2f688d38e22a10538d19ad079355f291744b11fd
Jacob Gonzalez <jacobgonzalez5252@gmail.com>
2020-10-08 15:12:36 +0000

    trigger

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 trait State {
-    fn next(self) -> Self;
+    type Trigger;
+
+    fn next(self, t: Self::Trigger) -> Self;
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Switch {
     Off,
-    On
+    HalfOn,
+    On,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Action {
+    FlickSwitch,
+    TurnSwitch,
 }
 
 impl State for Switch {
-    fn next(self) -> Self {
+    type Trigger = Action;
+    fn next(self, t: Self::Trigger) -> Self {
         match self {
-            Switch::Off => Switch::On,
+            Switch::Off => match t {
+                Action::FlickSwitch => Switch::On,
+                Action::TurnSwitch => Switch::HalfOn,
+            },
+            Switch::HalfOn => Switch::On,
             Switch::On => Switch::Off,
         }
     }
@@ -21,7 +35,7 @@ fn main() {
     let s = Switch::Off;
 
     println!("{:?}", s);
-    println!("{:?}", s.next());
-    println!("{:?}", s.next().next());
-    println!("{:?}", s.next().next().next());
+    println!("{:?}", s.next(Action::TurnSwitch));
+    println!("{:?}", s.next(Action::FlickSwitch));
+    println!("{:?}", s.next(Action::TurnSwitch).next(Action::TurnSwitch));
 }
