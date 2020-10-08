c69f834ffd392e4f7aabb5f8e3c08c00363e7f74
Jacob Gonzalez <jacobgonzalez5252@gmail.com>
2020-10-08 14:55:37 +0000

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+trait State {
+    fn next(self) -> Self;
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Switch {
+    Off,
+    On
+}
+
+impl State for Switch {
+    fn next(self) -> Self {
+        match self {
+            Switch::Off => Switch::On,
+            Switch::On => Switch::Off,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = Switch::Off;
+
+    println!("{:?}", s);
+    println!("{:?}", s.next());
+    println!("{:?}", s.next().next());
+    println!("{:?}", s.next().next().next());
 }
